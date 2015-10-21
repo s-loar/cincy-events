53a9b48bb0f8aa2ce4c94857f974886bede2c278
Steve Loar <sjl7909@gmail.com>
2015-10-21 10:16:40 -0400

    Add venue and event migrations

--- db/migrate/20151021134112_create_venues.rb
+++ db/migrate/20151021134112_create_venues.rb
@@ -1,7 +1,13 @@
 class CreateVenues < ActiveRecord::Migration
   def change
     create_table :venues do |t|
-
+      t.string :name
+      t.text :description
+      t.string :address
+      t.string :city
+      t.string :phone
+      t.decimal :lat,  precision: 12, scale: 6
+      t.decimal :lng, precision: 12, scale: 6
       t.timestamps null: false
     end
   end
--- db/migrate/20151021134309_create_events.rb
+++ db/migrate/20151021134309_create_events.rb
@@ -1,7 +1,12 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+      t.integer :venue_id
+      t.string :name
+      t.text :description
+      t.string :website
+      t.string :start_time
+      t.string :end_time
       t.timestamps null: false
     end
   end
